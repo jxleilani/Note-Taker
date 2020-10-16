c00cb2afe8db6597a5ea791b98c0bbc2798d60e6
jleilani <jxleilani@gmail.com>
2020-10-16 18:49:49 -0400

    writing to db.json file

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
+var dbjson = require("./db/db.json");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -7,21 +9,33 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//ROUTES
+//ROUTES-----------------------------------------------
 app.get('*', function(req,res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.get('/notes', function(req,res) {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
-//API ROUTES
+//API ROUTES--------------------------------------------
 app.get('/api/notes', function(req,res) {
-    
+    fs.readFile(dbjson, 'utf8', function(err,data) {
+
+    });
 });
 app.post('/api/notes', function(req,res) {
-    
+    fs.appendFile('./db/db.json', 'new post', function(err) {
+        if (err){
+            throw err;
+        }
+    });
+    res.json(dbjson);
 });
 app.delete('/api/notes/:id', function(req,res) {
     var noteId = req.params.id;
     console.log(noteId);
+});
+
+//START THE SERVER-------------------------------------
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
 });
